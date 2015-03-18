257a9c4bbcd3b0afd6c6f50b610a979682de64d3
Clemens Puehringer <cl.puehringer@gmail.com>
2015-03-18 18:08:49 +0100

    missed -c option for su

--- timelapse/install.sh
+++ timelapse/install.sh
@@ -12,7 +12,7 @@ CRONJOB_LINE="$CRONJOB_TIMING $CRONJOB_JOB"
 
 #takes a command to exectute as user pi
 function as_user {
-    su $DEFAULT_USER $1
+    su $DEFAULT_USER -c $1
 }
 
 #make timelapse.sh and cronjob.sh executable
@@ -22,4 +22,4 @@ as_user "chmod +x cronjob.sh"
 as_user "mkdir -p $LOCAL_PHOTO_DIR"
 
 #install a cronjob for the timelapse
-as_user "cronjob.sh $CRONJOB_LINE"
+as_user "(crontab -l ; echo "$CRONJOB_LINE") | sort - | uniq - | crontab -"
