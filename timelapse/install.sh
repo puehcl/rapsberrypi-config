14bb2f7c8153d2a67c87d5c15ea687feeeb084ab
Clemens Puehringer <cl.puehringer@gmail.com>
2015-03-20 13:33:50 +0000

    fixed issues automatic cronjob creation for timelapse now working

--- timelapse/install.sh
+++ timelapse/install.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-LOCAL_PHOTO_DIR=$HOME/timelapse
-LOCAL_CREDENTIALS_FILE=$HOME/credentials_timelapse.txt
-CURRENT_DIRECTORY=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 DEFAULT_USER=pi
+USER_HOME=/home/$DEFAULT_USER
+LOCAL_PHOTO_DIR=$USER_HOME/timelapse
+LOCAL_CREDENTIALS_FILE=$USER_HOME/credentials_timelapse.txt
+CURRENT_DIRECTORY=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 CRONJOB_TIMING="1 5,8,11,14,17 * * *"
 CRONJOB_JOB="$CURRENT_DIRECTORY/timelapse.sh $LOCAL_PHOTO_DIR $LOCAL_CREDENTIALS_FILE"
@@ -12,14 +13,20 @@ CRONJOB_LINE="$CRONJOB_TIMING $CRONJOB_JOB"
 
 #takes a command to exectute as user pi
 function as_user {
-    su $DEFAULT_USER -c $1
+    su $DEFAULT_USER -c "$1"
 }
 
+if [ "$(id -u)" != "0" ]; then
+    echo "You need to be root to run this script"
+    exit 1
+fi 
+
 #make timelapse.sh and cronjob.sh executable
-as_user "chmod +x timelapse.sh"
-as_user "chmod +x cronjob.sh"
+as_user "chmod +x $CURRENT_DIRECTORY/timelapse.sh"
+as_user "chmod +x $CURRENT_DIRECTORY/cronjob.sh"
 #create local directory as user pi
 as_user "mkdir -p $LOCAL_PHOTO_DIR"
 
 #install a cronjob for the timelapse
-as_user "(crontab -l ; echo "$CRONJOB_LINE") | sort - | uniq - | crontab -"
+echo "installing cronjob $CRONJOB_LINE"
+as_user "(crontab -l ; echo '$CRONJOB_LINE') | sort - | uniq - | crontab -"
